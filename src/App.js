c3cfb36f58798d5073273c8a07c976f8336ade9b
Nicholas Miller <86576814+n1999ck@users.noreply.github.com>
2024-10-31 21:03:06 +0000

    Continued working on form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import "halfmoon/css/halfmoon.min.css";
-
+import {useForm} from 'react-hook-form';
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
@@ -11,173 +11,248 @@ function App() {
     });
   }, []);
 
+  
+  const { register, handleSubmit } = useForm();
+  
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
         <p>The current time is {currentTime}.</p>
-          <form className='form' action='/api/upload' method='post' encType='multipart/form-data'>
-      
-            <h3>1. Demographics and Personal Information</h3>
-            <label>PatientID:</label>
-            <input type="number" name="patientID" required /><br />
+        
+        <form className="form" action="/api/upload" method="post" onSubmit={handleSubmit(onSubmit)}>
+          <h3 className="mt-4">Demographics and Personal Information</h3>
 
-            <label>Age:</label>
-            <input type="number" name="age" min="0" required /><br />
+          <div className="row mb-3 mb-sm-3">
+            <label className="form-label">PatientID:</label>
+            <input type="number" className="form-control" {...register("patientID", { required: true })} />
+          </div>
 
-            <label>Gender:</label>
-            <select name="gender" required>
-                <option value="0">Female</option>
-                <option value="1">Male</option>
-            </select><br />
+          <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Age:</label>
+            <input type="number" className="form-control" {...register("age", { min: 0, required: true })} />
+          </div>
 
-            <label>Ethnicity:</label>
-            <select name="ethnicity" required>
-                <option value="0">Ethnicity 0</option>
-                <option value="1">Ethnicity 1</option>
-                <option value="2">Ethnicity 2</option>
-                <option value="3">Ethnicity 3</option>
-            </select><br />
+          <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Gender:</label>
+            <select className="form-select" {...register("gender", { required: true })}>
+              <option value="0">Female</option>
+              <option value="1">Male</option>
+            </select>
+          </div>
 
-            <label>Education Level:</label>
-            <select name="educationLevel" required>
-                <option value="0">Level 0</option>
-                <option value="1">Level 1</option>
-                <option value="2">Level 2</option>
-                <option value="3">Level 3</option>
-            </select><br />
+          <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Ethnicity:</label>
+            <select className="form-select" {...register("ethnicity", { required: true })}>
+              <option value="0">Hispanic or Latino</option>
+              <option value="1">White (Non-Hispanic)</option>
+              <option value="2">Black or African American</option>
+              <option value="3">Asian</option>
+            </select>
+          </div>
 
-            <h3>2. Lifestyle and Behavior</h3>
-            <label>BMI:</label>
-            <input type="number" name="bmi" min="15" max="40" step="0.1" required /><br />
+          <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Education Level:</label>
+            <select className="form-select" {...register("educationLevel", { required: true })}>
+              <option value="0">Less than High School</option>
+              <option value="1">High School Diploma or GED</option>
+              <option value="2">Some College or Associate's Degree</option>
+              <option value="3">Bachelor’s Degree or Higher</option>
+            </select>
+          </div>
 
-            <label>Smoking:</label>
-            <select name="smoking" required>
+          <h3 className="mt-4">Lifestyle and Behavior</h3>
+
+          <div className="row mb-3 mb-sm-3">
+            <label className="col-sm-3 col-form-label col-form-label-lg mb-1 mb-sm-0">BMI:</label>
+            <div className="col-sm-9">
+              <input type="number" className="form-control" {...register("bmi", { min: 15, max: 40, step: 0.1, required: true })} />
+              </div>
+          </div>
+
+          <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Smoking:</label>
+            <select className="form-select" {...register("smoking", { required: true })}>
+              <option value="0">No</option>
+              <option value="1">Yes</option>
+            </select>
+          </div>
+
+          <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Alcohol Consumption:</label>
+            <input type="number" className="form-control" {...register("alcoholConsumption", { min: 0, max: 20, step: 0.1, required: true })} />
+          </div>
+
+          <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Physical Activity:</label>
+            <input type="number" className="form-control" {...register("physicalActivity", { min: 0, max: 10, step: 0.1, required: true })} />
+          </div>
+
+          <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Diet Quality:</label>
+            <input type="number" className="form-control" {...register("dietQuality", { min: 0, max: 10, step: 0.1, required: true })} />
+          </div>
+
+          <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Sleep Quality:</label>
+            <input type="number" className="form-control" {...register("sleepQuality", { min: 0, max: 10, step: 0.1, required: true })} />
+          </div>
+
+          <h3 className="mt-4">Medical History and Conditions</h3>
+          
+          <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Family History of Alzheimer's:</label>
+                <select name="familyHistoryAlzheimers" className="form-select" required>
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Cardiovascular Disease:</label>
+                <select name="cardiovascularDisease" className="form-select" required>
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Diabetes:</label>
+                <select name="diabetes" className="form-select" required>
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Depression:</label>
+                <select name="depression" className="form-select" required>
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Head Injury:</label>
+                <select name="headInjury" className="form-select" required>
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Hypertension:</label>
+                <select name="hypertension" className="form-select" required>
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Systolic BP:</label>
+                <input type="number" name="systolicBP" min="90" max="179" className="form-control" required />
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Diastolic BP:</label>
+                <input type="number" name="diastolicBP" min="60" max="119" className="form-control" required />
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Cholesterol Total:</label>
+                <input type="number" name="cholesterolTotal" min="150" max="300" step="0.1" className="form-control" required />
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Cholesterol LDL:</label>
+                <input type="number" name="cholesterolLDL" min="50" max="200" step="0.1" className="form-control" required />
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Cholesterol HDL:</label>
+                <input type="number" name="cholesterolHDL" min="20" max="100" step="0.1" className="form-control" required />
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+                <label className="form-label">Cholesterol Triglycerides:</label>
+                <input type="number" name="cholesterolTriglycerides" min="50" max="400" step="0.1" className="form-control" required />
+            </div>
+          <h3 className="mt-4">Cognitive and Functional Assessments</h3>
+            
+        <div className="row mb-3 mb-sm-3">
+        <label className="form-label">MMSE:</label>
+        <input type="number" className="form-control" {...register("mmse", { min: 0, max: 30, step: 0.1, required: true })} />
+        </div>
+          <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Functional Assessment:</label>
+            <input type="number" maxLength={80} className="form-control" {...register("functionalAssessment", { min: 0, max: 10, step: 0.1, required: true })} />
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Memory Complaints:</label>
+            <select className="form-select" {...register("memoryComplaints", { required: true })}>
                 <option value="0">No</option>
                 <option value="1">Yes</option>
-            </select><br />
+            </select>
+            </div>
 
-            <label>Alcohol Consumption:</label>
-            <input type="number" name="alcoholConsumption" min="0" max="20" step="0.1" required /><br />
-
-            <label>Physical Activity:</label>
-            <input type="number" name="physicalActivity" min="0" max="10" step="0.1" required /><br />
-
-            <label>Diet Quality:</label>
-            <input type="number" name="dietQuality" min="0" max="10" step="0.1" required /><br />
-
-            <label>Sleep Quality:</label>
-            <input type="number" name="sleepQuality" min="0" max="10" step="0.1" required /><br />
-
-            <h3>3. Medical History and Conditions</h3>
-            <label>Family History of Alzheimer's:</label>
-            <select name="familyHistoryAlzheimers" required>
+            <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Behavioral Problems:</label>
+            <select className="form-select" {...register("behavioralProblems", { required: true })}>
                 <option value="0">No</option>
                 <option value="1">Yes</option>
-            </select><br />
+            </select>
+            </div>
 
-            <label>Cardiovascular Disease:</label>
-            <select name="cardiovascularDisease" required>
+            <div className="row mb-3 mb-sm-3">
+            <label className="form-label">ADL:</label>
+            <input type="number" className="form-control" {...register("adl", { min: 0, max: 10, step: 0.1, required: true })} />
+            </div>
+
+            <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Confusion:</label>
+            <select className="form-select" {...register("confusion", { required: true })}>
                 <option value="0">No</option>
                 <option value="1">Yes</option>
-            </select><br />
+            </select>
+            </div>
 
-            <label>Diabetes:</label>
-            <select name="diabetes" required>
+            <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Disorientation:</label>
+            <select className="form-select" {...register("disorientation", { required: true })}>
                 <option value="0">No</option>
                 <option value="1">Yes</option>
-            </select><br />
+            </select>
+            </div>
 
-            <label>Depression:</label>
-            <select name="depression" required>
+            <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Personality Changes:</label>
+            <select className="form-select" {...register("personalityChanges", { required: true })}>
                 <option value="0">No</option>
                 <option value="1">Yes</option>
-            </select><br />
+            </select>
+            </div>
 
-            <label>Head Injury:</label>
-            <select name="headInjury" required>
+            <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Difficulty Completing Tasks:</label>
+            <select className="form-select" {...register("difficultyCompletingTasks", { required: true })}>
                 <option value="0">No</option>
                 <option value="1">Yes</option>
-            </select><br />
+            </select>
+            </div>
 
-            <label>Hypertension:</label>
-            <select name="hypertension" required>
+            <div className="row mb-3 mb-sm-3">
+            <label className="form-label">Forgetfulness:</label>
+            <select className="form-select" {...register("forgetfulness", { required: true })}>
                 <option value="0">No</option>
                 <option value="1">Yes</option>
-            </select><br />
-
-            <label>Systolic BP:</label>
-            <input type="number" name="systolicBP" min="90" max="179" required /><br />
-
-            <label>Diastolic BP:</label>
-            <input type="number" name="diastolicBP" min="60" max="119" required/><br />
-
-            <label>Cholesterol Total:</label>
-            <input type="number" name="cholesterolTotal" min="150" max="300" step="0.1" required/><br />
-
-            <label>Cholesterol LDL:</label>
-            <input type="number" name="cholesterolLDL" min="50" max="200" step="0.1" required/><br />
-
-            <label>Cholesterol HDL:</label>
-            <input type="number" name="cholesterolHDL" min="20" max="100" step="0.1" required/><br />
-
-            <label>Cholesterol Triglycerides:</label>
-            <input type="number" name="cholesterolTriglycerides" min="50" max="400" step="0.1" required/><br />
-
-            <h3>4. Cognitive and Functional Assessments</h3>
-            <label>MMSE:</label>
-            <input type="number" name="mmse" min="0" max="30" step="0.1" required/><br />
-
-            <label>Functional Assessment:</label>
-            <input type="number" name="functionalAssessment" min="0" max="10" step="0.1" required/><br />
-
-            <label>Memory Complaints:</label>
-            <select name="memoryComplaints" required>
-                <option value="0">No</option>
-                <option value="1">Yes</option>
-            </select><br />
-
-            <label>Behavioral Problems:</label>
-            <select name="behavioralProblems" required>
-                <option value="0">No</option>
-                <option value="1">Yes</option>
-            </select><br />
-
-            <label>ADL:</label>
-            <input type="number" name="adl" min="0" max="10" step="0.1" required /><br />
-
-            <label>Confusion:</label>
-            <select name="confusion" required>
-                <option value="0">No</option>
-                <option value="1">Yes</option>
-            </select><br />
-
-            <label>Disorientation:</label>
-            <select name="disorientation" required>
-                <option value="0">No</option>
-                <option value="1">Yes</option>
-            </select><br />
-
-            <label>Personality Changes:</label>
-            <select name="personalityChanges" required>
-                <option value="0">No</option>
-                <option value="1">Yes</option>
-            </select><br />
-
-            <label>Difficulty Completing Tasks:</label>
-            <select name="difficultyCompletingTasks" required>
-                <option value="0">No</option>
-                <option value="1">Yes</option>
-            </select><br />
-
-            <label>Forgetfulness:</label>
-            <select name="forgetfulness" required>
-                <option value="0">No</option>
-                <option value="1">Yes</option>
-            </select><br />
-
-            <input type="submit" value="Submit" />
-
-          </form>
+            </select>
+            </div>
+          <button type="submit" className="btn btn-primary mt-4">Submit</button>
+        </form>
       </header>
     </div>
   );
